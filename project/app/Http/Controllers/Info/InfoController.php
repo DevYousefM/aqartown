9720cc8a688dd5e3a2f553f75d3b631b83789ab0
devyousefm <devyousefm@gmail.com>
2024-10-08 20:22:06 +0300

    minimize return data

--- project/app/Http/Controllers/Info/InfoController.php
+++ project/app/Http/Controllers/Info/InfoController.php
@@ -11,7 +11,14 @@ class InfoController extends Controller
     public function products()
     {
         $products = Product::where('status', 1)->latest()->limit(6)->get();
-
+        // asset('assets/images/products/' . $productt->photo)
+        $products = $products->map(function ($product) {
+            return [
+                'id' => $product->id,
+                'name' => $product->name,
+                'image' => url('assets/images/products/' . $product->photo),
+            ];
+        });
         return response()->json($products);
     }
 }
