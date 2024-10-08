c97960458e33c9dc23fa29d67eae89dfce8942d1
devyousefm <devyousefm@gmail.com>
2024-10-08 20:55:07 +0300

    handle lang in products

--- project/app/Http/Controllers/Info/InfoController.php
+++ project/app/Http/Controllers/Info/InfoController.php
@@ -4,19 +4,20 @@ namespace App\Http\Controllers\Info;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use Illuminate\Http\Request;
 
 class InfoController extends Controller
 {
 
-    public function products()
+    public function products(Request $request)
     {
         $products = Product::where('status', 1)->latest()->limit(6)->get();
         $products = $products->map(function ($product) {
             return [
                 'id' => $product->id,
-                'name' => $product->name,
+                'name' => $request->lang == 'ar' ? $product->name_ar : $product->name,
                 'image' => url('assets/images/products/' . $product->photo),
-                'url' => route('front.product', ['slug' => $product->slug_ar, 'lang' => 'ar']),
+                'url' => route('front.product', ['slug' => $request->lang == 'ar' ? $product->slug_ar : $product->slug, 'lang' => $request->lang]),
                 'price' => $product->price
             ];
         });
