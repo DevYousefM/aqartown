1ca5b87d53a888a52a2fb0edf8f49b95236aff7d
devyousefm <devyousefm@gmail.com>
2024-10-10 18:13:28 +0300

    Update Laravel Version

--- project/app/Http/Controllers/Admin/GalleryController.php
+++ project/app/Http/Controllers/Admin/GalleryController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Gallery;
 use App\Models\Category;
+use App\Models\Product;
 use Image;
 
 class GalleryController extends Controller
@@ -20,94 +21,88 @@ class GalleryController extends Controller
     {
         $data[0] = 0;
         $id = $_GET['id'];
-        $prod = Category::findOrFail($id);
-        if(count($prod->galleries))
-        {
+        $prod = Product::findOrFail($id);
+        if (count($prod->galleries)) {
             $data[0] = 1;
             $data[1] = $prod->galleries;
         }
-        return response()->json($data);              
-    }    
+        return response()->json($data);
+    }
     public function mobileshow()
     {
         $data[0] = 0;
         $id = $_GET['id'];
         $prod = Category::findOrFail($id);
-        if(count($prod->mobilegalleries))
-        {
+        if (count($prod->mobilegalleries)) {
             $data[0] = 1;
             $data[1] = $prod->mobilegalleries;
         }
-        return response()->json($data);              
-    }  
+        return response()->json($data);
+    }
 
     public function store(Request $request)
-    { 
+    {
         $data = null;
         $lastid = $request->category_id;
         $proid = $request->product_id;
-        if ($files = $request->file('gallery')){
-            foreach ($files as  $key => $file){
+        if ($files = $request->file('gallery')) {
+            foreach ($files as  $key => $file) {
                 $val = $file->getClientOriginalExtension();
-                if($val == 'jpeg'|| $val == 'jpg'|| $val == 'png'|| $val == 'svg')
-                  {
+                if ($val == 'jpeg' || $val == 'jpg' || $val == 'png' || $val == 'svg') {
                     $gallery = new Gallery;
 
 
-        $img = Image::make($file->getRealPath())->resize(800, 800);
-        $thumbnail = time().str_random(8).'.jpg';
-        $img->save(public_path().'/assets/images/galleries/'.$thumbnail);
+                    $img = Image::make($file->getRealPath())->resize(800, 800);
+                    $thumbnail = time() . str_random(8) . '.jpg';
+                    $img->save(public_path() . '/assets/images/galleries/' . $thumbnail);
 
                     $gallery['photo'] = $thumbnail;
                     $gallery['category_id'] = $lastid;
                     $gallery['product_id'] = $proid;
                     $gallery->save();
-                    $data[] = $gallery;                        
-                  }
+                    $data[] = $gallery;
+                }
             }
         }
-        return response()->json($data);      
-    }   
-    
+        return response()->json($data);
+    }
+
     public function storemobile(Request $request)
-    { 
+    {
         $data = null;
         $lastid = $request->category_id;
         $proid = $request->product_id;
-        if ($files = $request->file('gallery')){
-            foreach ($files as  $key => $file){
+        if ($files = $request->file('gallery')) {
+            foreach ($files as  $key => $file) {
                 $val = $file->getClientOriginalExtension();
-                if($val == 'jpeg'|| $val == 'jpg'|| $val == 'png'|| $val == 'svg')
-                  {
+                if ($val == 'jpeg' || $val == 'jpg' || $val == 'png' || $val == 'svg') {
                     $gallery = new Gallery;
 
 
-        $img = Image::make($file->getRealPath())->resize(800, 800);
-        $thumbnail = time().str_random(8).'.jpg';
-        $img->save(public_path().'/assets/images/galleries/'.$thumbnail);
+                    $img = Image::make($file->getRealPath())->resize(800, 800);
+                    $thumbnail = time() . str_random(8) . '.jpg';
+                    $img->save(public_path() . '/assets/images/galleries/' . $thumbnail);
 
                     $gallery['photo'] = $thumbnail;
                     $gallery['category_id'] = $lastid;
                     $gallery['product_id'] = $proid;
                     $gallery['web'] = 0;
                     $gallery->save();
-                    $data[] = $gallery;                        
-                  }
+                    $data[] = $gallery;
+                }
             }
         }
-        return response()->json($data);      
-    } 
+        return response()->json($data);
+    }
 
     public function destroy()
     {
 
         $id = $_GET['id'];
         $gal = Gallery::findOrFail($id);
-            if (file_exists(public_path().'/assets/images/galleries/'.$gal->photo)) {
-                unlink(public_path().'/assets/images/galleries/'.$gal->photo);
-            }
+        if (file_exists(public_path() . '/assets/images/galleries/' . $gal->photo)) {
+            unlink(public_path() . '/assets/images/galleries/' . $gal->photo);
+        }
         $gal->delete();
-            
-    } 
-
+    }
 }
