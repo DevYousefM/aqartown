ef1a7feeef371820ec3a5501f2c6638b6610ae5c
Yousef Mohamed <devyousefm@gmail.com>
2025-07-11 16:31:14 +0300

    Fix 500 Error

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -27,15 +27,16 @@ class SecurityHeaders
 
         $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
 
-        $response->headers->set('Content-Security-Policy', "
-            default-src 'self';
-            script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://connect.facebook.net https://cdnjs.cloudflare.com https://www.googleadservices.com https://googleads.g.doubleclick.net https://www.google-analytics.com;
-            style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-            font-src 'self' https://fonts.gstatic.com data:;
-            img-src 'self' data: https://www.google-analytics.com https://www.googleadservices.com https://www.googletagmanager.com https://connect.facebook.net;
-            connect-src 'self' https://www.google-analytics.com https://www.googleadservices.com https://googleads.g.doubleclick.net;
-            frame-src 'self' https://td.doubleclick.net;
-        ");
+        $response->headers->set('Content-Security-Policy', implode('; ', [
+            "default-src 'self'",
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://connect.facebook.net https://cdnjs.cloudflare.com https://www.googleadservices.com https://googleads.g.doubleclick.net https://www.google-analytics.com",
+            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+            "font-src 'self' https://fonts.gstatic.com data:",
+            "img-src 'self' data: https://www.google-analytics.com https://www.googleadservices.com https://www.googletagmanager.com https://connect.facebook.net",
+            "connect-src 'self' https://www.google-analytics.com https://www.googleadservices.com https://googleads.g.doubleclick.net",
+            "frame-src 'self' https://td.doubleclick.net"
+        ]));
+
         $response->headers->set('Permissions-Policy', 'geolocation=(self), microphone=(), camera=(), fullscreen=(self)');
 
         return $response;
